346ade4806c5ebcb2a9cf09bf3c6f5f4cf67ac3c
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-06-02 11:31:31 +0100

    création du premiere version composants frontend

--- admin-dashboard_VFULL2/server/config/database.js
+++ admin-dashboard_VFULL2/server/config/database.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "hr_dashboard",
+  database: process.env.DB_NAME || "anep-fc",
   password: process.env.DB_PASSWORD || "password",
   port: process.env.DB_PORT || 5432,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
