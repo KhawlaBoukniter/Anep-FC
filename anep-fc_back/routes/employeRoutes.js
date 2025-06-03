9ffd75dbc77df49e236e1049f5f94ad4f6a47f4e
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-06-03 16:12:47 +0100

    Suppression des dossiers inutilisables

--- anep-fc_back/routes/employeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getEmployes } = require("../controllers/employeController");
-
-router.get("/", getEmployes);
-
-module.exports = router;
