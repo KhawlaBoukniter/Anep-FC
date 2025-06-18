4c73d9be6f9705d702b6aa97526aa31b97ecdf05
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-06-18 09:56:56 +0100

    l'ajout d'un box-shadow pour le side bare

--- admin-dashboard_VFULL2/client/src/components/ui/sidebar.tsx
+++ admin-dashboard_VFULL2/client/src/components/ui/sidebar.tsx
@@ -198,11 +198,10 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-white p-0 text-sidebar-foreground [&>button]:hidden shadow-lg" // Ajout de shadow-lg
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
-                backgroundColor: "white", // Style inline
               } as React.CSSProperties
             }
             side={side}
@@ -249,7 +248,7 @@ const Sidebar = React.forwardRef<
         >
           <div
             data-sidebar="sidebar"
-            className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
+            className="flex h-full w-full flex-col bg-sidebar shadow-xl" // shadow-xl ajouté ici
           >
             {children}
           </div>
