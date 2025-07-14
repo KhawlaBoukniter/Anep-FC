f2c882d881846b56633be0bda078ff2afc24c92d
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-07-14 14:08:00 +0100

    Resolve deployment errors

--- admin-dashboard_VFULL2/server/sequelize-config.js
+++ admin-dashboard_VFULL2/server/sequelize-config.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-    {
-        dialect: 'postgres',
-        logging: false,
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+    logging: false,
+});
 
 module.exports = sequelize;
