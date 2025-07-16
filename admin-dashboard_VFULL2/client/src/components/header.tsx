b412fc4ef9e8749362dd9e42edcf48324f05170c
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-07-16 10:21:24 +0100

    gestion des indisponibilités coté client

--- admin-dashboard_VFULL2/client/src/components/header.tsx
+++ admin-dashboard_VFULL2/client/src/components/header.tsx
@@ -133,13 +133,13 @@ const Header: React.FC = () => {
                                 {isUserMenuOpen && (
                                     <div className="absolute right-0 mt-1 w-48 bg-white shadow-lg rounded-lg py-2 z-50">
                                         <Link
-                                            to="/disponibilite"
+                                            to="/indisponibilite"
                                             className="flex items-center px-4 py-2 text-[#06668C] hover:bg-gray-100"
                                         >
                                             <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                             </svg>
-                                            Disponibilité
+                                            Indisponibilité
                                         </Link>
                                         <Link
                                             to="/formationPersonnel"
@@ -213,11 +213,11 @@ const Header: React.FC = () => {
                                         Formation
                                     </Link>
                                     <Link
-                                        to="/disponibilite"
+                                        to="/indisponibilite"
                                         className="text-[#06668C] font-bold duration-200"
                                         onClick={toggleMobileMenu}
                                     >
-                                        Disponibilité
+                                        Indisponibilité
                                     </Link>
                                     <Link
                                         to="/formationPersonnel"
